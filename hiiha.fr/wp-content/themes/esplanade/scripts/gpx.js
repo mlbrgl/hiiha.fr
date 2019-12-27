f30fb6612487759c2e521459fcbe12fd1d55ec7f
Matthieu Bergel <hello@matthieubergel.org>
2019-12-27 23:48:04 +0100

    format source

--- hiiha.fr/wp-content/themes/esplanade/scripts/gpx.js
+++ hiiha.fr/wp-content/themes/esplanade/scripts/gpx.js
@@ -1,18 +1,14 @@
 function displayGpxTrack(gpxUrl, mapId = "map") {
   var map = L.map(mapId);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
+    attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
   }).addTo(map);
   new L.GPX(gpxUrl, {
     async: true,
     marker_options: {
-      startIconUrl:
-        "http://github.com/mpetazzoni/leaflet-gpx/raw/master/pin-icon-start.png",
-      endIconUrl:
-        "http://github.com/mpetazzoni/leaflet-gpx/raw/master/pin-icon-end.png",
-      shadowUrl:
-        "http://github.com/mpetazzoni/leaflet-gpx/raw/master/pin-shadow.png"
+      startIconUrl: "http://github.com/mpetazzoni/leaflet-gpx/raw/master/pin-icon-start.png",
+      endIconUrl: "http://github.com/mpetazzoni/leaflet-gpx/raw/master/pin-icon-end.png",
+      shadowUrl: "http://github.com/mpetazzoni/leaflet-gpx/raw/master/pin-shadow.png"
     }
   })
     .on("loaded", function(e) {
